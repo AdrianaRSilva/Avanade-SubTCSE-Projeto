ba1da0448ad038bf36b069b8719b6cafbe591035
AdrianaRs <adrianarafael2010@gmail.com>
2021-06-15 09:42:53 -0300

    criaçao de pastas dentro da pasta Dominio e dentro da pasta crianda Entities foi criana uma class

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregate/EmployeeRoles/Entities/EmployeeRole.cs
@@ -0,0 +1,17 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregate.EmployeeRoles.Entities
+{
+    class EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public string Id { get; private set; }
+
+        public string RoleName { get; init; }
+
+    }
+
+}
