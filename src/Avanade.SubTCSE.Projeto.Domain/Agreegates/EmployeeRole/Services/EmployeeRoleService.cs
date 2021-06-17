d1e6b6940127e3b86789a0143ab6ed1b00e8079f
AdrianaRs <adrianarafael2010@gmail.com>
2021-06-17 10:18:59 -0300

    Emplemetação de Class

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Agreegates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -0,0 +1,12 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Agreegates.EmployeeRole.Interfaces.Services;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Agreegates.EmployeeRole.Services
+{
+    public class EmployeeRoleService : IEmployeeRoleService
+    {
+        public void AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
